9f12a8b6dfd5e6f683796ed77475dcf9e1cef2ad
Djeye <nick7@inbox.ru>
2022-11-07 20:30:32 +0300

    [refactor] clear code

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -8,17 +8,21 @@ namespace Character
         [SerializeField] private float speed;
         [SerializeField] private float turnSmoothTime;
 
-        private Transform _cam;
         private CharacterController _controller;
         private Transform _transform;
+        private Transform _cam;
 
         private float _angleVelocity;
-        
+
         private void Awake()
         {
             _controller = GetComponent<CharacterController>();
             _transform = GetComponent<Transform>();
-            _cam = Camera.main.transform;
+
+            if (Camera.main != null)
+            {
+                _cam = Camera.main.transform;
+            }
         }
 
         private void Start()
@@ -28,32 +32,41 @@ namespace Character
 
         private void Update()
         {
-            var horizontalAxis = Input.GetAxis("Horizontal");
-            var verticalAxis = Input.GetAxis("Vertical");
-            
-            Vector3 moveVector = new Vector3(horizontalAxis, 0, verticalAxis).normalized;
+            Moving();
+        }
 
-            if (moveVector.magnitude < 0.1f)
+
+        private void Moving()
+        {
+            if (!InputController.Instance.IsMoving)
             {
                 return;
             }
-            
-            var targetAngle = Mathf.Atan2(moveVector.x, moveVector.z) * Mathf.Rad2Deg + _cam.transform.eulerAngles.y;
-            
-            var angle = Mathf.SmoothDampAngle(_transform.eulerAngles.y, targetAngle, ref _angleVelocity,
+
+            Vector2 moveVector = InputController.Instance.MoveVector;
+            float targetAngle = Mathf.Atan2(moveVector.x, moveVector.y) * Mathf.Rad2Deg + _cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(_transform.eulerAngles.y, targetAngle, ref _angleVelocity,
                 turnSmoothTime);
-            
-            _transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            
-            var moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            
-            if (!_controller.isGrounded)
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            ApplyGravity(ref moveDirection);
+            ApplyMovement(angle, moveDirection);
+        }
+
+        private void ApplyGravity(ref Vector3 moveDirection)
+        {
+            if (_controller.isGrounded)
             {
-                moveDirection += Physics.gravity;
+                return;
             }
 
-            var moveSpeed = Time.deltaTime * speed;
-            _controller.Move(moveSpeed * moveDirection.normalized);
+            moveDirection += Physics.gravity;
+        }
+
+        private void ApplyMovement(float angle, Vector3 moveDirection)
+        {
+            _transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            _controller.Move(Time.deltaTime * speed * moveDirection.normalized);
         }
     }
 }
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public static InputController Instance { get; private set; }
+
+    public Vector2 MoveVector { get; private set; }
+    public bool IsMoving => MoveVector.magnitude >= 0.1f;
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    private void Update()
+    {
+        GetInputAxis();
+    }
+    
+
+    private void GetInputAxis()
+    {
+        var horizontalAxis = Input.GetAxis("Horizontal");
+        var verticalAxis = Input.GetAxis("Vertical");
+
+        MoveVector = new Vector2(horizontalAxis, verticalAxis).normalized;
+    }
+}
